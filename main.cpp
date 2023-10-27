1bd8e905a947232381cc3a50d54cec78aa93bbe3
German <snipe9029quant@gmail.com>
2023-10-27 19:58:38 +0300

    Hello, World

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by German on 27.10.2023.
+//
+#include "openmpi_hello.h"
+
+int main() {
+    print_processors();
+    return 0;
+}
---
+++ openmpi_hello.h
@@ -0,0 +1,9 @@
+//
+// Created by German on 27.10.2023.
+//
+#ifndef OPENMPI_TEST_OPENPMI_HELLO_H
+#define OPENMPI_TEST_OPENPMI_HELLO_H
+
+void print_processors();
+
+#endif //OPENMPI_TEST_OPENPMI_HELLO_H
